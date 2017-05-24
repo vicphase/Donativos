56cf63f8b12087d26847b51b5f8c85a3a44e9c6e
Victor Martínez <Victor Martnez>
2017-05-23 21:21:24 -0500

    Agregando comas a los SQL

--- api/sqlScripts/table-colonia.sql
+++ api/sqlScripts/table-colonia.sql
@@ -1,8 +1,8 @@
 CREATE TABLE colonia (
   id INT NOT NULL AUTO_INCREMENT,
   nombre varchar(255) NOT NULL,
-  idSector INT NOT NULL
-  PRIMARY KEY (id)
+  idSector INT NOT NULL,
+  PRIMARY KEY (id),
   FOREIGN KEY (idSector)
         REFERENCES sector(id)
 )
--- api/sqlScripts/table-sector.sql
+++ api/sqlScripts/table-sector.sql
@@ -1,8 +1,8 @@
 CREATE TABLE sector (
   id INT NOT NULL AUTO_INCREMENT,
     nombre varchar(255) NOT NULL,
-  idZona INT NOT NULL
-   PRIMARY KEY (id)
+  idZona INT NOT NULL,
+   PRIMARY KEY (id),
    FOREIGN KEY (idZona)
         REFERENCES zona(id)
 )
