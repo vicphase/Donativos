a48cde1bca826fd1ff50fdf5bb9d907900c0e147
amartinez354 <albertomarg22@gmail.com>
2017-05-24 13:19:07 -0500

    Add files via upload

---
+++ api/beans/Usuario.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Victor
+ * Date: 4/7/2017
+ * Time: 2:17 PM
+ */
+class BienHechor
+{
+
+    public $id;
+    public $username;
+    public $password;
+
+    /**
+     * BienHechor constructor.
+     */
+    public function __construct()
+    {
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+	
+	/**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+	
+	/**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+}
