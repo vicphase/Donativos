8d64247495a346747c0091b0f04234db3c5679a3
Victor Martínez <Victor Martnez>
2017-05-24 05:09:56 -0500

    Templates bienhechor

---
+++ app/scripts/controllers/bienhechor/editarbienhechor.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name donativosApp.controller:EditarbienhechorCtrl
+ * @description
+ * # EditarbienhechorCtrl
+ * Controller of the donativosApp
+ */
+angular.module('donativosApp')
+  .controller('EditarbienhechorCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
